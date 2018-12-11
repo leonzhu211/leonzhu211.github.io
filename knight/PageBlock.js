e1f231a6665f78141b21dfd44eed6cb75b83a4a7
leonzhu211 <leonzhu211@qq.com>
2018-12-11 04:06:39 +0000

    wrapper

--- knight/PageBlock.js
+++ knight/PageBlock.js
@@ -1,30 +1,30 @@
-function PageBlockBase4x4(){
+function PageBlockBase4x4() {
     this.b = new Board(4, 4);
     this.k = new Knight();
     this.k.bind(this.b);
 
     this.multiPath = new MultiPath();
 
-    this.startPosArr = [P(0,0), P(0,1), P(0,2), P(0,3)];
+    this.startPosArr = [P(0, 0), P(0, 1), P(0, 2), P(0, 3)];
     this.colorArr = ['blue', 'green', 'red', 'black']
 
-    this.getPath = function(posIndex){
+    this.getPath = function (posIndex) {
         var path = new Path();
         var startPos = this.startPosArr[posIndex];
         var currentPos = startPos;
         path.push(currentPos);
         var dirIndex = 0;
-        while(true){
+        while (true) {
             var pos = currentPos.next(RULE.getDir(dirIndex));
-            if(this.b.isValidPosition(pos)){
+            if (this.b.isValidPosition(pos)) {
                 currentPos = pos;
                 path.push(currentPos);
-                if(currentPos.equal(startPos)){
+                if (currentPos.equal(startPos)) {
                     break;
                 }
             }
             dirIndex++;
-            if(dirIndex == RULE.length){
+            if (dirIndex == RULE.length) {
                 dirIndex = 0;
             }
         }
@@ -32,55 +32,57 @@ function PageBlockBase4x4(){
         return path;
     }
 
-    this.getAllPath = function(){
-        for(var i = 0; i < this.startPosArr.length; i++){
+    this.getAllPath = function () {
+        for (var i = 0; i < this.startPosArr.length; i++) {
             this.multiPath.push(this.getPath(i));
         }
     }
 
-    this.init = function(){
+    this.init = function () {
         this.getAllPath();
         this.multiPath.bind(this.b);
     }
 
-    this.showBoard = function(x, y){
+    this.showBoard = function (x, y) {
         this.b.show(x, y);
     }
 
-    this.show = function(x, y){
-        this.showBoard(x,y);
+    this.show = function (x, y) {
+        this.showBoard(x, y);
         this.multiPath.show(this.colorArr);
     }
 
-    this.animate = function(x, y){
-        this.showBoard(x,y);
+    this.animate = function (x, y) {
+        this.showBoard(x, y);
         this.multiPath.animate(this.colorArr);
     }
 
     this.init();
 }
 
-var PageBlockBase4x4Animate = _PageAnimateWrapper(PageBlockBase4x4);
+function PageBlockBase4x4Animate() {
+    return _PageAnimateWrapper(PageBlockBase4x4)();
+}
 
-function PageBlockDoubleCyleToLine(){
+function PageBlockDoubleCyleToLine() {
     this.b = new Board(4, 4);
     this.base = new PageBlockBase4x4();
     this.multiPath = new MultiPath();
     this.colorArr = [];
 
-    this.init = function(){
+    this.init = function () {
         var multiPath = this.base.multiPath;
-        for(var i = 0; i < multiPath.getLength(); i+=2){
+        for (var i = 0; i < multiPath.getLength(); i += 2) {
             var p1 = multiPath.getPath(i);
-            var p2 = multiPath.getPath(i+1);
-            var path = p1.mergeCycle(P(0,i), p2, P(0,i+1));
+            var p2 = multiPath.getPath(i + 1);
+            var path = p1.mergeCycle(P(0, i), p2, P(0, i + 1));
             this.multiPath.push(path);
             this.colorArr.push(this.base.colorArr[i]);
         }
         this.multiPath.bind(this.b);
     }
 
-    this.show = function(x, y){
+    this.show = function (x, y) {
         this.base.b.show(x, y);
         this.multiPath.bind(this.base.b);
         this.base.multiPath.getPath(0).show('red', 50);
@@ -92,25 +94,25 @@ function PageBlockDoubleCyleToLine(){
     this.init();
 }
 
-function PageBlockDoubleLine4x4(){
+function PageBlockDoubleLine4x4() {
     this.b = new Board(4, 4);
     this.base = new PageBlockBase4x4();
     this.multiPath = new MultiPath();
     this.colorArr = [];
 
-    this.init = function(){
+    this.init = function () {
         var multiPath = this.base.multiPath;
-        for(var i = 0; i < multiPath.getLength(); i+=2){
+        for (var i = 0; i < multiPath.getLength(); i += 2) {
             var p1 = multiPath.getPath(i);
-            var p2 = multiPath.getPath(i+1);
-            var path = p1.mergeCycle(P(0,i), p2, P(0,i+1));
+            var p2 = multiPath.getPath(i + 1);
+            var path = p1.mergeCycle(P(0, i), p2, P(0, i + 1));
             this.multiPath.push(path);
             this.colorArr.push(this.base.colorArr[i]);
         }
         this.multiPath.bind(this.b);
     }
 
-    this.show = function(x, y){
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
@@ -118,13 +120,13 @@ function PageBlockDoubleLine4x4(){
     this.init();
 }
 
-function PageBlockDoubleLine8x4FlipX(){
+function PageBlockDoubleLine8x4FlipX() {
     this.base = new PageBlockDoubleLine4x4();
     this.b = new Board(8, 4);
     this.multiPath = new MultiPath();
     this.colorArr = this.base.colorArr;
 
-    this.init = function(){
+    this.init = function () {
         var multiPath = this.base.multiPath;
         var multiPathLeft = multiPath.clone();
         var multiPathRight = multiPath.clone();
@@ -140,7 +142,7 @@ function PageBlockDoubleLine8x4FlipX(){
         this.multiPath.pushMultiPath(multiPathRight);
     }
 
-    this.show = function(x, y){
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
@@ -148,13 +150,13 @@ function PageBlockDoubleLine8x4FlipX(){
     this.init();
 }
 
-function PageBlockDoubleCycle8x4(){
+function PageBlockDoubleCycle8x4() {
     this.base = new PageBlockDoubleLine8x4FlipX();
     this.b = new Board(8, 4);
     this.multiPath = new MultiPath();
     this.colorArr = this.base.colorArr;
-    
-    this.init = function(){
+
+    this.init = function () {
         var multiPath = this.base.multiPath;
         this.multiPath.push(multiPath.getPath(0).mergeLine(multiPath.getPath(3)));
         this.multiPath.push(multiPath.getPath(1).mergeLine(multiPath.getPath(2)));
@@ -162,114 +164,114 @@ function PageBlockDoubleCycle8x4(){
         this.multiPath.getPath(1).turnToCycle();
         this.multiPath.bind(this.b);
     }
-    
-    this.show = function(x, y){
+
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
-    
+
     this.init();
 }
 
-function PageBlockSingleLine8x4Top(){
+function PageBlockSingleLine8x4Top() {
     this.base = new PageBlockDoubleCycle8x4();
     this.b = new Board(8, 4);
     this.multiPath = new MultiPath();
     this.colorArr = this.base.colorArr;
-    
-    this.init = function(){
+
+    this.init = function () {
         var multiPath = this.base.multiPath;
         var path0 = multiPath.getPath(0);
         var path1 = multiPath.getPath(1);
-        var path = path0.mergeCycle(P(0,3), path1, P(1,3));
+        var path = path0.mergeCycle(P(0, 3), path1, P(1, 3));
         this.multiPath.push(path);
         this.multiPath.bind(this.b);
     }
-    
-    this.show = function(x, y){
+
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
-    
+
     this.init();
 }
 
-function PageBlockSingleLine8x4Bottom(){
+function PageBlockSingleLine8x4Bottom() {
     this.base = new PageBlockDoubleCycle8x4();
     this.b = new Board(8, 4);
     this.multiPath = new MultiPath();
     this.colorArr = this.base.colorArr;
-    
-    this.init = function(){
+
+    this.init = function () {
         var multiPath = this.base.multiPath;
         multiPath.flipY();
         var path0 = multiPath.getPath(0);
         var path1 = multiPath.getPath(1);
-        var path = path0.mergeCycle(P(2,0), path1, P(3,0));
+        var path = path0.mergeCycle(P(2, 0), path1, P(3, 0));
         this.multiPath.push(path);
         this.multiPath.bind(this.b);
     }
-    
-    
-    this.show = function(x, y){
+
+
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
-    
+
     this.init();
 }
 
-function PageBlockDoubleLine8x8(){
+function PageBlockDoubleLine8x8() {
     this.baseTop = new PageBlockSingleLine8x4Top();
     this.baseBottom = new PageBlockSingleLine8x4Bottom();
-    
+
     this.b = new Board(8, 8);
     this.multiPath = new MultiPath();
     this.colorArr = this.baseTop.colorArr;
-    
-    this.init = function(){
+
+    this.init = function () {
         var multiPathTop = this.baseTop.multiPath;
         var multiPathBottom = this.baseBottom.multiPath;
         multiPathTop.bind(this.b);
         multiPathBottom.bind(this.b);
-        multiPathBottom.move(D(0,4));
+        multiPathBottom.move(D(0, 4));
         this.multiPath.pushMultiPath(multiPathTop);
         this.multiPath.pushMultiPath(multiPathBottom);
         this.multiPath.bind(this.b);
     }
-    
-    
-    this.show = function(x, y){
+
+
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
-    
+
     this.init();
 }
 
-function PageBlockSingleCycle8x8(){
+function PageBlockSingleCycle8x8() {
     this.base = new PageBlockDoubleLine8x8();
     this.b = new Board(8, 8);
     this.multiPath = new MultiPath();
     this.colorArr = this.base.colorArr;
-    
-    this.init = function(){
+
+    this.init = function () {
         var multiPath = this.base.multiPath;
         var path0 = multiPath.getPath(0);
         var path1 = multiPath.getPath(1);
         var path = path0.mergeLine(path1);
         path.turnToCycle();
-        
+
         this.multiPath.push(path);
         this.multiPath.bind(this.b);
     }
-    
-    
-    this.show = function(x, y){
+
+
+    this.show = function (x, y) {
         this.b.show(x, y);
         this.multiPath.show(this.colorArr);
     }
-    
+
     this.init();
 }
 
