42a488196e27d8d0d26a22d85a3dc50e571e6dec
leonzhu211 <leonzhu211@qq.com>
2018-12-24 07:27:22 +0000

    menu size

--- knight/Menu.js
+++ knight/Menu.js
@@ -1,17 +1,17 @@
-function Request(){
-    var url=location.search;
+function Request() {
+    var url = location.search;
     var r = new Object();
-    if(url.indexOf("?")!=-1){
+    if (url.indexOf("?") != -1) {
         var str = url.substr(1);
-        var strs= str.split("&");
-        for(var i= 0; i < strs.length; i++){
-            r[strs[i].split("=")[0]]=unescape(strs[ i].split("=")[1]);
+        var strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            r[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
         }
     }
     return r;
 }
 
-function Menu(){
+function Menu() {
     this.pageArr = [];
     this.sourceArr = [];
     this.current = '';
@@ -23,134 +23,133 @@ function Menu(){
     this.init();
 }
 
-Menu.prototype.initPageArr = function(){
+Menu.prototype.initPageArr = function () {
     var pageReg = /^Page/;
-    for(var n in window){
-        if(pageReg.exec(n) && window[n] instanceof Function){
+    for (var n in window) {
+        if (pageReg.exec(n) && window[n] instanceof Function) {
             this.pageArr.push(n.replace(pageReg, ''));
         }
     }
-    if(this.pageArr.length > 0){
+    if (this.pageArr.length > 0) {
         this.type = 'page';
         this.current = this.pageArr[0];
     }
 }
 
-Menu.prototype.initSourceArr = function(){
+Menu.prototype.initSourceArr = function () {
     var scripts = $('SCRIPT');
     var self = this;
-    scripts.each(function(){
-        var s = this.src.substr(this.src.lastIndexOf('/')+1);
-        if(s[0] >= 'A' && s[0] <= 'Z'){
+    scripts.each(function () {
+        var s = this.src.substr(this.src.lastIndexOf('/') + 1);
+        if (s[0] >= 'A' && s[0] <= 'Z') {
             s = s.substr(0, s.lastIndexOf('.'));
             self.sourceArr.push(s);
         }
     });
 }
 
-Menu.prototype.initCurrent = function(){
+Menu.prototype.initCurrent = function () {
     var req = Request();
-    if(typeof(req.op)!='undefined'){
+    if (typeof (req.op) != 'undefined') {
         this.current = req.op;
     }
-    if(typeof(req.type)!='undefined'){
+    if (typeof (req.type) != 'undefined') {
         this.type = req.type;
     }
 }
 
-Menu.prototype.initPageList = function(page, head){
+Menu.prototype.initPageList = function (page, head) {
     this.liPage = $('<li />').appendTo(this.ul);
-    this.headPage = $("<a class='head'>"+head+"</a>").appendTo(this.liPage);
+    this.headPage = $("<a class='head'>" + head + "</a>").appendTo(this.liPage);
     this.ulPage = $('<ul />').appendTo(this.liPage);
-    var re = new RegExp('^'+page);
-    for(var i = 0; i < this.pageArr.length; i++){
+    var re = new RegExp('^' + page);
+    for (var i = 0; i < this.pageArr.length; i++) {
         var title = this.pageArr[i];
-        if(re.exec(title)){
+        if (re.exec(title)) {
             var li = $('<li />');
             li.appendTo(this.ulPage);
             var a = $('<a />');
-            if(title==this.current && this.type=='page'){
+            if (title == this.current && this.type == 'page') {
                 a.addClass('curr');
             }
             a.appendTo(li);
-            a.attr('href', '?type=page&op='+title);
+            a.attr('href', '?type=page&op=' + title);
             a.text(title.replace(re, ''));
         }
     }
 }
 
-Menu.prototype.init = function(){
+Menu.prototype.init = function () {
     this.container = $("<div />")
-        .css('visibility','hidden')
+        .css('visibility', 'hidden')
         .attr('id', 'navigation')
+        .css('height', '600px')
         .appendTo('body');
-    
-    
+
+
     this.ul = $('<ul />');
     this.ul.appendTo(this.container);
-    
+
     this.initPageList('Base', '基本信息');
     this.initPageList('Block', '分块');
     this.initPageList('Cycle', '分环');
-    
-    
-    this.liSource= $('<li />').appendTo(this.ul);
+
+
+    this.liSource = $('<li />').appendTo(this.ul);
     this.headSource = $("<a class='head'>源代码</a>").appendTo(this.liSource);
     this.ulSource = $('<ul />').appendTo(this.liSource);
-    
-    for(var i = 0; i < this.sourceArr.length; i++){
+
+    for (var i = 0; i < this.sourceArr.length; i++) {
         var title = this.sourceArr[i];
         var li = $('<li />');
         li.appendTo(this.ulSource);
         var a = $('<a />');
-        if(title==this.current && this.type=='source'){
+        if (title == this.current && this.type == 'source') {
             a.addClass('curr');
         }
         a.appendTo(li);
-        a.attr('href', '?type=source&op='+title);
-        a.text(title+'.js');
+        a.attr('href', '?type=source&op=' + title);
+        a.text(title + '.js');
     }
-    
-    $('a').mouseover(function(){
+
+    $('a').mouseover(function () {
         $(this).addClass('focus');
     });
-    $('a').mouseout(function(){
+    $('a').mouseout(function () {
         $(this).removeClass('focus');
     });
 }
 
-Menu.prototype.show = function(x, y){
+Menu.prototype.show = function (x, y) {
     this.x = x;
     this.y = y;
     this.container.css('left', this.x);
     this.container.css('top', this.y);
-    this.container.css('visibility','visible');
+    this.container.css('visibility', 'visible');
     this.ul.accordion({
-		active: true,
-		header: '.head',
-		navigation: true,
-		event: 'click',
-		fillSpace: true
-	});
-	
-	var pageClass = null;
+        active: true,
+        header: '.head',
+        navigation: true,
+        event: 'click',
+        fillSpace: true
+    });
+
+    var pageClass = null;
     var page = null;
     var margin = 0;
 
-    if(this.type == 'page'){
+    if (this.type == 'page') {
         var pageName = 'Page' + this.current;
-        if(typeof(window[pageName])!='undefined'){
+        if (typeof (window[pageName]) != 'undefined') {
             pageClass = window[pageName];
-        }
-        else{
+        } else {
             error(pageName + ' not found');
             return;
         }
         margin = 20;
-    }
-    else if(this.type == 'source'){
+    } else if (this.type == 'source') {
         pageClass = _PageSource(this.current);
-        if(!pageClass){
+        if (!pageClass) {
             error(pageName + ' not found');
             return;
         }
@@ -158,11 +157,10 @@ Menu.prototype.show = function(x, y){
     }
 
     page = new pageClass();
-    
-    if(page){
-        page.show(($(document).width() - page.b.width())/2, y+margin);
-    }
-    else{
+
+    if (page) {
+        page.show(($(document).width() - page.b.width()) / 2, y + margin);
+    } else {
         error('wrong param');
     }
 }
