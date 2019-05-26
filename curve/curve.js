fe813d37fa035a23024c74f11a1977575b2f9e0b
leonzhu211 <leonzhu211@qq.com>
2019-05-26 13:06:47 +0100

    simple ui

--- curve/curve.js
+++ curve/curve.js
@@ -4,30 +4,26 @@ function Curve() {
 
     this.x = 50;
     this.y = 50;
-    this.stepx = 10;
-    this.stepy = 4;
-    this.interval = 10;
 
+    this.interval = 10;
     this.theta = 0;
     this.step = 0.001;
-    this.times = 200;
-    this.R = 200;
-    this.r = 101;
-    this.d = 49;
+
+    this.times = 1000;
+    this.R = 400;
+    this.r = parseInt(this.R / 2) + 7;
+    this.d = parseInt(this.r / 2) + 7;
 
     this.a1 = 1.0;
     this.a2 = 1.0;
     this.a3 = 1.0;
     this.a4 = 1.0;
 
-    this.color = '#000000';
+    this.color = '#ff0000';
 
     this.container = $('#container');
     this.btnPlay = $('#btnPlay');
 
-    window.defaultstart = function () {
-        self.onclickDefaultStart();
-    }
     window.start = function () {
         self.onclickStart();
     }
@@ -35,48 +31,52 @@ function Curve() {
     window.randomstart0 = function () {
         self.onclickRandomStart0()
     }
-    window.randomstart12 = function () {
-        self.onclickRandomStart12()
-    }
-    window.randomstart34 = function () {
-        self.onclickRandomStart34()
-    }
-    window.randomstart13 = function () {
-        self.onclickRandomStart13()
-    }
-    window.randomstart24 = function () {
-        self.onclickRandomStart24()
-    }
-
     window.randomstart1234 = function () {
-        self.onclickRandomStart24()
+        self.onclickRandomStart1234()
     }
-}
 
-Curve.prototype.onclickDefaultStart = function () {
-    $('#R').val(300);
-    $('#r').val(113);
-    $('#d').val(51);
-    $('#color').val('#ff0000');
-    $('#a1').val(1.0);
-    $('#a2').val(1.0);
-    $('#a3').val(1.0);
-    $('#a4').val(1.0);
     this.onclickStart();
 }
 
-Curve.prototype.random0 = function () {
-    this.R = parseInt($('#R').val());
-    var r = this.randInt(1, this.R);
-    var d = this.randInt(1, r);
-    $('#r').val(r);
-    $('#d').val(d);
-    $('#color').val(this.randColor());
-    $('#a1').val(1.0);
-    $('#a2').val(1.0);
-    $('#a3').val(1.0);
-    $('#a4').val(1.0);
+Curve.prototype.paramToHash = function () {
+    var hash = '#main';
+    var arr = [this.times, this.r, this.d, this.a1, this.a2, this.a3, this.a4, this.color];
+    for (var i = 0; i < arr.length; i++) {
+        hash += '/' + arr[i];
+    }
+    window.location.href = hash;
 }
+
+Curve.prototype.hashToParam = function () {
+    var hash = window.location.hash;
+    var arr = hash.split('/');
+    console.log(arr);
+    if (arr.length != 9) {
+        console.log('arr length error');
+        return;
+    }
+    var i = 1;
+    this.times = parseFloat(arr[i++]);
+    this.r = parseFloat(arr[i++]);
+    this.d = parseFloat(arr[i++]);
+    this.a1 = parseFloat(arr[i++]);
+    this.a2 = parseFloat(arr[i++]);
+    this.a3 = parseFloat(arr[i++]);
+    this.a4 = parseFloat(arr[i++]);
+    this.color = arr[i++];
+}
+
+
+Curve.prototype.random0 = function () {
+    this.r = this.randInt(1, this.R);
+    this.d = this.randInt(1, this.r);
+    this.color = this.randColor();
+    this.a1 = 1.0;
+    this.a2 = 1.0;
+    this.a3 = 1.0;
+    this.a4 = 1.0;
+}
+
 Curve.prototype.onclickRandomStart0 = function () {
     this.random0();
     this.onclickStart();
@@ -84,46 +84,21 @@ Curve.prototype.onclickRandomStart0 = function () {
 
 Curve.prototype.randArg = function () {
     // return this.randInt(1, 4);
-    return 5 * Math.random();
+    return Math.round(5 * Math.random() * 100) / 100;
 }
 
-Curve.prototype.onclickRandomStartX = function (x) {
-    this.random0()
-    var arr = "1234";
-    var i;
-    for (i = 0; i < arr.length; i++) {
-        var n = arr[i];
-        if (x.indexOf(n) != -1) {
-            $('#a' + n).val(this.randArg());
-        }
-        else {
-            $('#a' + n).val(1.0);
-        }
-    }
-    this.onclickStart();
-}
-
-Curve.prototype.onclickRandomStart12 = function () {
-    this.onclickRandomStartX('12');
-}
-
-Curve.prototype.onclickRandomStart34 = function () {
-    this.onclickRandomStartX('34');
-}
-
-Curve.prototype.onclickRandomStart13 = function () {
-    this.onclickRandomStartX('13');
-}
-
-Curve.prototype.onclickRandomStart24 = function () {
-    this.onclickRandomStartX('24');
-}
 Curve.prototype.onclickRandomStart1234 = function () {
-    this.onclickRandomStartX('1234');
+    this.random0();
+    this.a1 = this.randArg();
+    this.a2 = this.randArg();
+    this.a3 = this.randArg();
+    this.a4 = this.randArg();
+    this.onclickStart();
 }
 
 Curve.prototype.onclickStart = function () {
     var self = this;
+    this.paramToHash();
     this.init();
     this.start();
     var running = true;
@@ -159,16 +134,6 @@ Curve.prototype.randInt = function (Min, Max) {
 }
 
 Curve.prototype.init = function () {
-    this.R = parseInt($('#R').val());
-    this.r = parseInt($('#r').val());
-    this.d = parseInt($('#d').val());
-    this.times = parseInt($('#speed').val());
-    this.color = $('#color').val();
-    this.a1 = parseFloat($('#a1').val());
-    this.a2 = parseFloat($('#a2').val());
-    this.a3 = parseFloat($('#a3').val());
-    this.a4 = parseFloat($('#a4').val());
-
     this.theta = 0;
     clearInterval(this.timer);
     this.m = Math.max(this.R, this.r, this.d);
